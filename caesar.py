a7db766e82ef949552524ff6d2e1e3b82910d265
Alekky09 <Alekky09@users.noreply.github.com>
2019-12-06 02:19:46 +0000

    automated commit by check50 [check50=True]

--- caesar.py
+++ caesar.py
@@ -8,6 +8,7 @@ k = int(argv[1])
 
 if k > 25:
     k = k % 25
+
 print(k)
 print("plaintext:", end = "")
 pt = get_string(" ")
@@ -15,9 +16,14 @@ pt = get_string(" ")
 print("ciphertext: ", end = "")
 
 for char in pt:
+
     if char.isalpha():
         char = chr(ord(char) + k)
-    print(char, end="")
+
+        if not char.isalpha():
+            char = chr(ord(char) - 25)
+
+     print(char, end="")
 print("")
 
 
